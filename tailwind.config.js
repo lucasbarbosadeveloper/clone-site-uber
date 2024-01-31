e3995c9684a92c00798b38f27c0ee42cd1773511
lucasbarbosadeveloper <lucasbarbosa20k@gmail.com>
2024-01-31 01:57:43 -0300

    feat: 'header' mobile finalized

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        secondary: "#D1D5DB"     
+      }
+    },
   },
   plugins: [],
 }
